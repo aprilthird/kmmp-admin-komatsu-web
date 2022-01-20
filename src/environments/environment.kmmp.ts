fd9589f76ffc29f6865de112e7e2f697fabfe2e3
carlos <carlos.alfonso2.7.9@gmail.com>
2022-01-20 10:02:10 -0500

    [update]: kmmp container path

--- src/environments/environment.kmmp.ts
+++ src/environments/environment.kmmp.ts
@@ -8,7 +8,7 @@ export const environment = {
   title: "KMMP",
   apiUrl: "https://development-kmp.ws.solera.pe/api",
   azureAccountName: "appinformes",
-  azureContaineName: "dcp",
+  azureContaineName: "kmmp",
   azureSas:
     "sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-03T23:39:46Z&st=2022-01-03T15:39:46Z&spr=https&sig=BU4Y9BHhsNT4EbY%2FM2eJZ7X9EYdNE4NUC9nbcSwFuc8%3D",
 };
