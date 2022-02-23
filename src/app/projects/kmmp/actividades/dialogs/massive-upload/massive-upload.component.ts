82e878af025f1a75193a6ca7d2bd02e79280e277
carlos <carlos.alfonso2.7.9@gmail.com>
2022-02-22 19:25:39 -0500

    [feat]: massive upload

--- src/app/projects/kmmp/actividades/dialogs/massive-upload/massive-upload.component.ts
+++ src/app/projects/kmmp/actividades/dialogs/massive-upload/massive-upload.component.ts
@@ -11,6 +11,8 @@ import { ActivitiesService } from "../../activities.service";
 export class MassiveUploadComponent implements OnInit {
   //file = new FormControl(Validators.required);
   file = "";
+  fileFormat =
+    "https://appinformes.blob.core.windows.net/kmmp/carga%20masiva?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-03T23:39:46Z&st=2022-01-03T15:39:46Z&spr=https&sig=BU4Y9BHhsNT4EbY%2FM2eJZ7X9EYdNE4NUC9nbcSwFuc8%3D";
 
   constructor(private _activitiesService: ActivitiesService) {}
 
