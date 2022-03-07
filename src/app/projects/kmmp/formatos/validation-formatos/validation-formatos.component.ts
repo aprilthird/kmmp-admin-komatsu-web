780615301b3538e031171b3879b4ccad324c282e
carlos <carlos.alfonso2.7.9@gmail.com>
2022-03-07 10:33:51 -0500

    [improvements]: allow managing field type sign

--- src/app/projects/kmmp/formatos/validation-formatos/validation-formatos.component.ts
+++ src/app/projects/kmmp/formatos/validation-formatos/validation-formatos.component.ts
@@ -560,12 +560,15 @@ export class ValidationFormatosComponent implements OnInit {
           if (parametro.activo) {
             if (
               parametro.idParametro === TipoParametro.IMAGEN ||
-              parametro.idParametro === TipoParametro.UPLOAD
+              parametro.idParametro === TipoParametro.UPLOAD ||
+              parametro.idParametro === TipoParametro.FIRMA
             ) {
               this.checkImgParam(parametro, j, k);
-            } else if (parametro.idParametro === TipoParametro.FIRMA) {
-              this.checkSignParam(paramIdx, parametro, indexGroup, k, j);
-            } else if (parametro.idParametro === TipoParametro.FECHA) {
+            }
+            // else if (parametro.idParametro === TipoParametro.FIRMA) {
+            //   this.checkSignParam(paramIdx, parametro, indexGroup, k, j);
+            // }
+            else if (parametro.idParametro === TipoParametro.FECHA) {
               // if (typeof parametro.valor === "string") {
               //   parametro.valor = this.form.get(
               //     this.getParametroControl({ i, j, k })
